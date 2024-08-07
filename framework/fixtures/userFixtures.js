10d763a2e997782a8af51a210c001d59638bb11d
motyatrof <136427962+motyatrof@users.noreply.github.com>
2024-08-07 14:48:52 +0900

    fixt

--- framework/fixtures/userFixtures.js
+++ framework/fixtures/userFixtures.js
@@ -1,4 +1,9 @@
-export * as UserFixture from './userFixture'
 
+import { faker } from '@faker-js/faker'
 
-
+export function generateUserCredentials() {
+  return {
+    userName: faker.internet.email(),
+    password: 'P@ssw0rd',
+  }
+}
